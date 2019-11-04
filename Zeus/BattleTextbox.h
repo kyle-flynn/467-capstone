5eefc6d79276e59b1c0dddc81b05d28e85ae1286
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-11-03 22:38:16 -0500

    Added text that automatically newlines.
    Based upon a simple algorithm that checks if its text width is greater than its textbox width.

--- Zeus/BattleTextbox.h
+++ Zeus/BattleTextbox.h
@@ -9,9 +9,12 @@ class BattleTextbox : public sf::Drawable, sf::Transformable {
 public:
 	BattleTextbox();
 	void update(float deltaTime);
+	void appendText(const std::string& text);
 private:
 	sf::Texture textboxTexture;
 	sf::Sprite textboxSprite;
+	sf::Text battleText;
+	void checkForNewlines(const std::string& text);
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
